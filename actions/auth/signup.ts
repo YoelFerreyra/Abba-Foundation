893c8d02f6e02c02b4b27b438349873311204b3b
EricFerreyra <yoelferreya24@gmail.com>
2025-06-11 18:00:11 -0300

    feat: enhance user registration and login; add validation for passwords and patient lookup; refactor schemas and update database structure

--- actions/auth/signup.ts
+++ actions/auth/signup.ts
@@ -1,29 +1,63 @@
-"use server"
+"use server";
 import { authServer } from "@/lib/firebase/firebase-server";
 import { prisma } from "@/lib/prisma";
 
 type RegisterUserData = {
   email: string;
   password: string;
+  confirmPassword: string;
+  dni: string;
+  dniTramite: string;
 };
 
 export async function registerUserAction(data: RegisterUserData) {
   try {
+    if (data.password !== data.confirmPassword)
+      throw new Error("The passwords are not equals");
+
+    const patient = await prisma.patient.findFirst({
+      where: {
+        dni: data.dni,
+        isActive: true,
+        patientStatus: "ACTIVE",
+        dniProcessingNumber: data.dniTramite,
+      },
+    });
+
+    if (!patient) throw new Error("Patient not found");
+
     const firebaseUser = await authServer.createUser({
       email: data.email,
       password: data.password,
       //displayName: data.name,
     });
+
+    /*
+    const actionCodeSettings = {
+      url: "https://tusitio.com/verificacion-completada",
+      handleCodeInApp: true,
+    };
+
+    const verificationLink = await authServer.generateEmailVerificationLink(
+      data.email,
+      actionCodeSettings
+    );
     
+    console.log(verificationLink);
+    */
+
     const user = await prisma.user.create({
       data: {
         firebaseUid: firebaseUser.uid,
         email: data.email,
         //name: data.name,
-        role: "MEMBER",
-        image: "https://avatar.iran.liara.run/public/2"
+        role: "CLIENT",
+        patient: {
+          connect: { id: patient.id }
+        },
+        //image: "https://avatar.iran.liara.run/public/2",
       },
-    });
+    });    
 
     return { ok: true };
   } catch (error: any) {
--- app/login/schemas/login-schema.ts
+++ app/login/schemas/login-schema.ts
@@ -1,17 +1,8 @@
-// schemas/register-schema.ts
 import { z } from "zod";
 
-export const registerSchema = z
-  .object({
-    email: z.string().email("Correo inválido"),
-    password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
-    confirmPassword: z.string(),
-    dni: z.string().min(7, "DNI inválido").max(10),
-    dniTramite: z.string().min(7, "Número de trámite inválido").max(12),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Las contraseñas no coinciden",
-    path: ["confirmPassword"],
-  });
+export const loginSchema = z.object({
+  email: z.string().email("Correo inválido"),
+  password: z.string().min(1, "Contraseña requerida"),
+});
 
-export type RegisterFormInputs = z.infer<typeof registerSchema>;
+export type LoginFormInputs = z.infer<typeof loginSchema>;
